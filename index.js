74dff3502b844ea54218b80ba4f1086f9683c783
tusku8 <tusku8@gmail.com>
2025-05-30 19:16:29 +0800

    fix

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+export { emaBot, chatBot } from './app.js';
+export { getState, session_store } from './session_store.js';
+
 // index.js
 export function greet(name) {
     return `Hello, ${name}! 👋`;
